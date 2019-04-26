d53a8c373f05d497bfb97fcfc6245dabb1e5c607
Gnyana teja <cb.en.u4cse16247@cb.students.amrita.edu>
2019-04-27 04:44:08 +0530

    make it betterrr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
-import {environment} from '../environments/environment';
+
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import {FormsModule} from '@angular/forms';
-
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyCsiTkDAW2cFcRYPDtZHYGxnadt3xl-ZoI",
+    authDomain: "appointmenter-10074.firebaseapp.com",
+    databaseURL: "https://appointmenter-10074.firebaseio.com",
+    projectId: "appointmenter-10074",
+    storageBucket: "appointmenter-10074.appspot.com",
+    messagingSenderId: "736348785150"
+  }
+};
 
 @NgModule({
   declarations: [
@@ -17,7 +27,7 @@ import {FormsModule} from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule.initializeApp(environment),
  	  AngularFirestoreModule,
     AppRoutingModule,
     AngularFireDatabaseModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,4 @@
 // The list of file replacements can be found in `angular.json`.
 
  // Initialize Firebase
- export const environment = {
-  firebase : {
-    apiKey: "AIzaSyCsiTkDAW2cFcRYPDtZHYGxnadt3xl-ZoI",
-    authDomain: "appointmenter-10074.firebaseapp.com",
-    databaseURL: "https://appointmenter-10074.firebaseio.com",
-    projectId: "appointmenter-10074",
-    storageBucket: "appointmenter-10074.appspot.com",
-    messagingSenderId: "736348785150"
-  }
-};
+ 
