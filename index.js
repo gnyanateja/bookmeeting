21e7f9eafd13127c3801d5ad5ea13c9205b2ea85
Gnyana teja <cb.en.u4cse16247@cb.students.amrita.edu>
2019-04-27 04:38:47 +0530

    make it betterrr

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/pmail'));
+app.use(express.static(__dirname + '/dist/appoint'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/pmail/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/appoint/index.html'));
 });
 
 // default Heroku port
